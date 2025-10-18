7a43a89cda6e479ebc757f717fdbeeb36082d88b
= <=>
2025-10-18 10:22:03 +0200

    fix img

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -8,31 +8,31 @@ export interface Sponsor {
 export const sponsors: Sponsor[] = [
   {
     name: "Google",
-    logo: "/sponsor/googleLogo.png",
+    logo: "/devFest/sponsor/googleLogo.png",
     website: "https://google.com",
     tier: "Main"
   },
   {
     name: "Immobiliare Labs",
-    logo: "/sponsor/immoLogo.png",
+    logo: "/devFest/sponsor/immoLogo.png",
     website: "https://labs.immobiliare.it/",
     tier: "Platinum"
   },
   {
     name: "Ready2Use",
-    logo: "/sponsor/readyLogo.png",
+    logo: "/devFest/sponsor/readyLogo.png",
     website: "https://r2u.it/",
     tier: "Gold"
   },
   {
     name: "Bizzy Now",
-    logo: "/sponsor/bizzylogo.png",
+    logo: "/devFest/sponsor/bizzylogo.png",
     website: "https://www.bizzynow.com/",
     tier: "Silver"
   },
   {
     name: "DatWave",
-    logo: "/sponsor/datLogo.png",
+    logo: "/devFest/sponsor/datLogo.png",
     website: "https://datwave.ai/",
     tier: "Silver"
   },
