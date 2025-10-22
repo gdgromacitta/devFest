44dfc41bebde32130ace415e286c5feac5cf30ef
= <=>
2025-10-22 20:29:56 +0200

    Fixed Partner Logo

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -9,49 +9,49 @@ export interface Sponsor {
 export const sponsors: Sponsor[] = [
   {
     name: "Google",
-    logo: "/devFest/sponsor/googleLogo.png",
+    logo: "/sponsor/googleLogo.png",
     website: "https://google.com",
     tier: "Main",
     text: ""
   },
   {
     name: "Immobiliare Labs",
-    logo: "/devFest/sponsor/immoLogo.png",
+    logo: "/sponsor/immoLogo.png",
     website: "https://labs.immobiliare.it/",
     tier: "Platinum",
     text: ""
   },
   {
     name: "Ready2Use",
-    logo: "/devFest/sponsor/readyLogo.png",
+    logo: "/sponsor/readyLogo.png",
     website: "https://r2u.it/",
     tier: "Gold",
     text: ""
   },
   {
     name: "Bizzy Now",
-    logo: "/devFest/sponsor/bizzylogo.png",
+    logo: "/sponsor/bizzylogo.png",
     website: "https://www.bizzynow.com/",
     tier: "Silver",
     text: ""
   },
   {
     name: "DatWave",
-    logo: "/devFest/sponsor/datLogo.png",
+    logo: "/sponsor/datLogo.png",
     website: "https://datwave.ai/",
     tier: "Silver",
     text: ""
   },
   {
     name: "SharpCoding",
-    logo: "/devFest/sponsor/sharpCoding.png",
+    logo: "/sponsor/sharpCoding.png",
     website: "https://sharpcoding.it/",
     tier: "Partner",
     text: "Main"
   },
   {
     name: "BacatoTech",
-    logo: "/devFest/sponsor/bacaroTech.png",
+    logo: "/sponsor/bacaroTech.png",
     website: "https://bacarotech.github.io/",
     tier: "Partner",
     text: "Media"
